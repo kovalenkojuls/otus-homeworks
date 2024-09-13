73faf112248c05b9f0c2f414d3debaaadf0d07c6
kovalenkojuls <kovalenko_jul_s@mail.ru>
2024-09-13 10:08:07 +0300

    init homework 18

---
+++ hw18-spring-webflux/client-service/src/main/java/ru/otus/config/WsPrincipal.java
@@ -0,0 +1,17 @@
+package ru.otus.config;
+
+import java.security.Principal;
+
+public class WsPrincipal implements Principal {
+
+    private final String name;
+
+    public WsPrincipal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
